37c020e89e7badd2f34d0baf9b0d4c6ae651f755
wouter075 <whezel@novacollege.nl>
2026-02-19 09:13:24 +0100

    Added buttons

--- ShoundSharp/Form1.Designer.cs
+++ ShoundSharp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShoundSharp
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(643, 28);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(124, 29);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "+";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += button1_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(643, 63);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(124, 29);
+            btnEdit.TabIndex = 1;
+            btnEdit.Text = "Aanpassen";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(643, 98);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(124, 29);
+            btnDelete.TabIndex = 2;
+            btnDelete.Text = "Verwijderen";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Name = "frmMain";
+            Text = "SoundSharp";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
     }
 }
--- ShoundSharp/Form1.cs
+++ ShoundSharp/Form1.cs
@@ -1,10 +1,15 @@
 namespace ShoundSharp
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
